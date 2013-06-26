5e0d0def442ea53c0ba4b029ccef965e6e445265
sanni <sanni@station40>
2013-06-26 12:39:25 +0530

    initial commit

---
+++ testjava/src/TestJava.java
@@ -0,0 +1,14 @@
+public class TestJava {
+
+	public static void main(String[] args) {
+
+		String test1 = ".";
+
+		if (test1.equals(".")) {
+
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+	}
+}
